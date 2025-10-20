d2f9f9b2cecac10b0db084b53a22fdd5f59ad7e3
maramboussalem <marammaramboussalem@gmail.com>
2025-10-20 10:50:17 +0100

    Update pipes

--- src/app/shared/pipes/date-pers.pipe.ts
+++ src/app/shared/pipes/date-pers.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePersPipe implements PipeTransform {
  //
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
 
+
+
+  transform(value: any): string {
+    if (!value) return '';
+
+    const date = new Date(value);
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'short',   // lun, mar, mer...
+      day: 'numeric',     // 20
+      month: 'long',      // octobre
+      year: 'numeric'     // 2025
+    };
+
+    // Utilise la locale française pour afficher les noms corrects
+    return date.toLocaleDateString('fr-FR', options);
+  }
 }
